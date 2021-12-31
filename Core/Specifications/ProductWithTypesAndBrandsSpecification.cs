beba52819c934253619f36eb973a0dca2e136ca0
Assaf43 <himantz43@gmail.com>
2021-12-31 15:08:39 +0200

    Api Paging,Filter,Sorting,Searching

--- Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -6,10 +6,33 @@ namespace Core.Specifications
 {
     public class ProductWithTypesAndBrandsSpecification : BaseSpecifications<Product>
     {
-        public ProductWithTypesAndBrandsSpecification()
+        public ProductWithTypesAndBrandsSpecification(ProductSpecParams productParams)
+            : base(x =>
+               (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+               (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+               (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1), productParams.PageSize);
+
+            if (!String.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.price);
+                        break;
+                    default:
+                        AddOrderBy(p => p.price);
+                        break;
+                }
+            }
         }
 
         public ProductWithTypesAndBrandsSpecification(int id)
