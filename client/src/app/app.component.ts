b51acc5be4e8fd97a0f5ed192d0cb593644db94f
Assaf43 <himantz43@gmail.com>
2022-01-27 16:50:24 +0200

    Client - Identity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/product';
@@ -15,12 +16,30 @@ export class AppComponent implements OnInit {
   title = 'MyShop';
 
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');    
+    this.accountService.loadCurrentUser(token).subscribe(
+      () => {
+        console.log('user In');          
+      },
+      error => {
+        console.log(error);          
+      }
+    );
+  }
+
+  loadBasket() {
     const baskerId = localStorage.getItem('basket_id');
     if (baskerId) {
       this.basketService.getBasket(baskerId).subscribe(() => {
+        console.log('In Basket');
       }), error => {
         console.log(error);
       }
